c85c7dbcebcb88a914ff055d57e4206199fb64f0
Rajihkharissuha12 <rajhk704@gmail.com>
2023-07-19 05:11:00 +0700

    Problem 2

---
+++ problem2.ts
@@ -0,0 +1,12 @@
+function largestNumber(array:number[]) {
+    // your code here
+    const sort = array.sort((a,b)=>b-a)
+    return sort[0]
+  }
+  
+  
+  console.log(largestNumber([5, 2, 67, 37, 85, 19, 10])) // 85
+  console.log(largestNumber([5, 10, 20, 3, 98, 95])) // 98
+  console.log(largestNumber([20, 22, 18, 25, 75, 62, 88])) // 88
+  console.log(largestNumber([6, 23, 9, 5])) // 23
+  console.log(largestNumber([70, 44, 28, 18, 55, 68, 11])) // 70
